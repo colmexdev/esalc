783032ebe5d6fe333cf61921bd29dc3aeb0f6c3d
colmexdev <github@colmex.mx>
2018-04-16 17:23:04 -0500

    Panel

---
+++ app/models/admin.rb
@@ -0,0 +1,16 @@
+class Admin < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  validates_presence_of :usuario
+end
