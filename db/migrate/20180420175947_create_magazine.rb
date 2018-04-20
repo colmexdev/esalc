90fbff975e0619cb3250030244efbe18f395bf2e
colmexdev <github@colmex.mx>
2018-04-20 13:44:16 -0500

    Tabla revistas

---
+++ db/migrate/20180420175947_create_magazine.rb
@@ -0,0 +1,13 @@
+class CreateMagazine < ActiveRecord::Migration
+  def change
+    create_table :magazines do |t|
+      t.text :articulo, null: false
+      t.text :autores, null: false
+      t.text :volumen
+      t.text :numero
+      t.text :fecha
+      t.text :tematica
+      t.text :link, null: false
+    end
+  end
+end
