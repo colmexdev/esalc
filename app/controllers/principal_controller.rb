28b1af5ab0dd6138dcb8477183f71859476a0545
colmexdev <github@colmex.mx>
2018-04-23 16:41:05 -0500

    Acceso a eventos

--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -1,21 +1,9 @@
 class PrincipalController < ApplicationController
   def principal
-    @datos = []
-    begin
-      cliente = TinyTds::Client.new username: 'agendaPRED', password: '@g3NDa#', host: '172.16.40.220', port: '1433'
-      @resultado = cliente.execute("USE Agenda")
-      @resultado.do
-      @resultado = cliente.execute("SELECT * FROM dbo.vw_EventosCategoria ORDER BY PARSE(fechaInicio AS DATE USING 'es-ES') ASC, horaInicio ASC")
-      #@resultado = cliente.execute("SELECT * FROM dbo.vw_EventosCategoria WHERE DATEADD(day, DATEDIFF(day,'19000101',PARSE(fechaFin AS DATE USING 'es-ES')), CAST(horaFin AS DATETIME2(1))) >= CAST(GETDATE() AS DATETIME2(1))  ORDER BY PARSE(fechaInicio AS DATE USING 'es-ES') ASC, horaInicio ASC")
-      @resultado.each do |r|
-        @datos << r
-      end
-    rescue
-      @datos = {}
-    end
+    consulta_eventos
     respond_to do |format|
       format.html
-      format.json {render json: {videos: @resultado}}
+      format.json {render json: {videos: @datos}}
     end
   end
 
@@ -37,6 +25,26 @@ class PrincipalController < ApplicationController
     @unit = "pub-colmex"
     respond_to do |format|
       format.html { render :catalogo}
+    end
+  end
+
+  protected
+
+  def consulta_eventos
+    @datos = []
+    limit = params[:limit] || "ALL"
+    offset = params[:offset] || "0"
+    begin
+      cliente = TinyTds::Client.new username: 'agendaPRED', password: '@g3NDa#', host: '172.16.40.220', port: '1433'
+      @resultado = cliente.execute("USE Agenda")
+      @resultado.do
+      @resultado = cliente.execute("SELECT * FROM dbo.vw_EventosCategoria ORDER BY PARSE(fechaInicio AS DATE USING 'es-ES') ASC, horaInicio ASC OFFSET " + offset + " ROWS FETCH NEXT " + limit + " ROWS ONLY")
+      #@resultado = cliente.execute("SELECT * FROM dbo.vw_EventosCategoria WHERE DATEADD(day, DATEDIFF(day,'19000101',PARSE(fechaFin AS DATE USING 'es-ES')), CAST(horaFin AS DATETIME2(1))) >= CAST(GETDATE() AS DATETIME2(1))  ORDER BY PARSE(fechaInicio AS DATE USING 'es-ES') ASC, horaInicio ASC")
+      @resultado.each do |r|
+        @datos << r
+      end
+    rescue
+      @datos = {}
     end
   end
 end
