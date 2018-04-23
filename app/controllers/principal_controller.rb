0c9649ff58b1492ee0b6a131fbe6b1469bce4ae5
colmexdev <github@colmex.mx>
2018-04-23 16:19:27 -0500

    Acceso a eventos

--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -5,7 +5,7 @@ class PrincipalController < ApplicationController
       cliente = TinyTds::Client.new username: 'agendaPRED', password: '@g3NDa#', host: '172.16.40.220', port: '1433'
       @resultado = cliente.execute("USE Agenda")
       @resultado.do
-      @resultado = cliente.execute("SELECT * FROM dbo.vw_EventosCategoria WHERE LOWER(CATEGORIA) LIKE '%estudios sobre américa latina%' ORDER BY PARSE(fechaInicio AS DATE USING 'es-ES') ASC, horaInicio ASC")
+      @resultado = cliente.execute("SELECT * FROM dbo.vw_EventosCategoria ORDER BY PARSE(fechaInicio AS DATE USING 'es-ES') ASC, horaInicio ASC")
       #@resultado = cliente.execute("SELECT * FROM dbo.vw_EventosCategoria WHERE DATEADD(day, DATEDIFF(day,'19000101',PARSE(fechaFin AS DATE USING 'es-ES')), CAST(horaFin AS DATETIME2(1))) >= CAST(GETDATE() AS DATETIME2(1))  ORDER BY PARSE(fechaInicio AS DATE USING 'es-ES') ASC, horaInicio ASC")
       @resultado.each do |r|
         @datos << r
