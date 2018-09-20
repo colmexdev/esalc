2537f60c50e554d445544f342ceeb248b61dfba2
colmexdev <github@colmex.mx>
2018-09-20 16:32:32 -0500

    Primer lote de páginas

--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -12,33 +12,39 @@ class PrincipalController < ApplicationController
   end
 
   def historia
+    @tema = "Historia"
   end
 
   def lengua_literatura
+    @tema = "Lengua y literatura"
     respond_to do |format|
       format.html {render :historia}
     end
   end
 
   def partidos_politicos
+    @tema = "Política, partidos e instituciones"
     respond_to do |format|
       format.html {render :historia}
     end
   end
 
   def relaciones_internacionales
+    @tema = "Relaciones internacionales"
     respond_to do |format|
       format.html {render :historia}
     end
   end
 
   def sociedad_economia
+    @tema = "Sociedad y economía"
     respond_to do |format|
       format.html {render :historia}
     end
   end
 
   def violencia
+    @tema = "Violencia y Derechos Humanos"
     respond_to do |format|
       format.html {render :historia}
     end
