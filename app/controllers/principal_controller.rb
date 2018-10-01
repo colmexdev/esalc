385631d3b7d78b5600a19c89f9f580ce6344d5d9
colmexdev <github@colmex.mx>
2018-10-01 17:51:32 -0500

    Filtros

--- app/controllers/principal_controller.rb
+++ app/controllers/principal_controller.rb
@@ -22,7 +22,7 @@ class PrincipalController < ApplicationController
   end
 
   def videoteca
-    @url = "?" + (params.key?(:offset) ? ("offset=" + params[:offset]) : "") + (params.key?(:conds) ? (params[:conds].key?(:tema) ? "&tema=" + params[:conds][:tema] : "") : "")
+    @url = "?" + (params.key?(:offset) ? ("offset=" + params[:offset]) : "") + (params.key?(:conds) ? ((params[:conds].key?(:tema) ? "&tema=" + params[:conds][:tema] : "") + (params[:conds].key?(:autor) ? "&autor=" + params[:conds][:autor] : "") + (params[:conds].key?(:pais) ? "&pais=" + params[:conds][:pais] : "")) : "")
     respond_to do |format|
       format.html
       format.js
