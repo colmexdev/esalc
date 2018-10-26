05789db14620790d6212dcb7d1fefa3e28725c9a
colmexdev <github@colmex.mx>
2018-10-26 15:18:46 -0500

    Portadas

--- app/models/publicacion.rb
+++ app/models/publicacion.rb
@@ -5,4 +5,6 @@ class Publicacion < ActiveRecord::Base
                     :url => "/assets/portadas/:id_:basename.:extension",
                     :path => ":rails_root/public/assets/portadas/:id_:basename.:extension",
                     :default_url => "/vacio.png"
+
+  validates_attachment_content_type :foto, :content_type => ['image/jpeg', 'image/png', 'image/jpg']
 end
