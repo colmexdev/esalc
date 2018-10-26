10018c2b788c145e5278821633ea3e4c18a8d45a
colmexdev <github@colmex.mx>
2018-10-26 15:13:44 -0500

    Portadas

--- app/models/publicacion.rb
+++ app/models/publicacion.rb
@@ -1,7 +1,7 @@
 class Publicacion < ActiveRecord::Base
   self.table_name = "publicacions"
 
-  has_attached_file :foto, :styles => {},
+  has_attached_file :portada, :styles => {},
                     :url => "/assets/portadas/:id_:basename.:extension",
                     :path => ":rails_root/public/assets/portadas/:id_:basename.:extension",
                     :default_url => "/vacio.png"
--- app/models/publicacion.rb~
+++ app/models/publicacion.rb~
@@ -1,3 +1,8 @@
 class Publicacion < ActiveRecord::Base
   self.table_name = "publicacions"
+
+  has_attached_file :foto, :styles => {},
+                    :url => "/assets/portadas/:id_:basename.:extension",
+                    :path => ":rails_root/public/assets/portadas/:id_:basename.:extension",
+                    :default_url => "/vacio.png"
 end
