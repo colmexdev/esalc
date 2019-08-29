3be1955a36ef1eee2d4f2d46995502042976f3ad
colmexdev <github@colmex.mx>
2019-08-28 19:09:41 -0500

    Boletines

--- app/models/boletin.rb
+++ app/models/boletin.rb
@@ -2,10 +2,11 @@ class Boletin < ActiveRecord::Base
 
   validates_presence_of :titulo
   validates_presence_of :fecha
-  has_attached_file :documento, :styles => {:pdf_thumbnail => ["", :jpg], :original => ""},
+  has_attached_file :documento, :styles => {:pdf_thumbnail => ["", :jpg]},
                     :url => "/assets/boletines/:style/:id_:basename.:extension",
                     :path => ":rails_root/public/assets/boletines/:style/:id_:basename.:extension",
-                    :default_url => "/vacio.png" 
+                    :default_url => "/vacio.png",
+                    :convert_options => {:all => '-background white -flatten +matte'} 
 
   validates_attachment_content_type :documento, :content_type => ['application/pdf']
   validates_attachment_presence :documento
